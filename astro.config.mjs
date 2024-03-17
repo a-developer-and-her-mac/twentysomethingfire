fe62013e79bf9f216acdfa2441aea0a8e5297293
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2024-03-16 17:09:24 -0700

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a-developer-and-her-mac.github.io',
+  site: 'www.twentysomethingfire.com',
 });
